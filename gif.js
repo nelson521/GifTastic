5c96bb644231d4a6955521284b160cf36b7e2e73
Stevenson Nelson <sonson521@gmail.com>
2019-06-25 18:34:49 -0400

    fixed button

--- gif.js
+++ gif.js
@@ -2,14 +2,25 @@ $(document).ready(function() {
   var things = ["playing Pool", "photograpy", "scarlett johansson", "chimichanga", "rick and morty"];
 
   function makingButton() {
-    $('.showButton').empty();
+    $('#showButton').empty();
 
     for (var i = 0; i < things.length; i++) {
       var btn = $("<button>");
       btn.text(things[i]);
-      $(".showButton").append(btn);
+      $("#showButton").append(btn);
     }
-  }
+
+    $("#submitButton").on('click', function(event) {
+      event.preventDefault();
+      
+      // value from the input
+      var favList = $("#thingsList").val();
+      // add to fav list
+      things.push(favList);
+      makingButton();
+      
+    });
+  };
   makingButton();
   
 
