5ccacdefc93c362a13a883709dbf4d1fee4a6835
Stevenson Nelson <sonson521@gmail.com>
2019-06-24 23:44:28 -0400

    added a button for loop.

---
+++ gif.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var things = ["playing Pool", "photograpy", "scarlett johansson", "chimichanga", "rick and morty"];
+
+  function makingButton() {
+    $('.showButton').empty();
+
+    for (var i = 0; i < things.length; i++) {
+      var btn = $("<button>");
+      btn.text(things[i]);
+      $(".showButton").append(btn[i]);
+    }
+  }
+  makingButton();
+  
+
+
+
+
+
+
+
+
+});
