c32f8b02b3f38db54565e8536cd9b6299b4d166e
Stevenson Nelson <sonson521@gmail.com>
2019-06-28 19:55:26 -0400

    added func

--- gif.js
+++ gif.js
@@ -1,29 +1,34 @@
-$(document).ready(function() {
-  var things = ["playing Pool", "photograpy", "scarlett johansson", "chimichanga", "rick and morty"];
-
+$(document).ready(function () {
+  var things = ["playing pool", "photograpy", "scarlett johansson", "chimichanga", "rick and morty"];
+  var key = "https://api.giphy.com/v1/gifs/search?api_key=74onGORbRcVBdg55CFw1tMcsKREsHiwv&q=playing pool&limit=10&offset=0&rating=PG-13&lang=en;";
+  
   function makingButton() {
-    $('#showButton').empty();
+    $('#showButtons').empty();
 
     for (var i = 0; i < things.length; i++) {
       var btn = $("<button>");
+      btn.addClass("gifButton");
       btn.text(things[i]);
-      $("#showButton").append(btn);
+      btn.attr('data-search', things[i]);
+      $("#showButtons").append(btn);
     }
 
-    $("#submitButton").on('click', function(event) {
-      event.preventDefault();
-      
-      // value from the input
-      var favList = $("#thingsList").val();
-      // add to fav list
-      things.push(favList);
-      makingButton();
-      
-    });
+
+    
   };
   makingButton();
-  
 
+  $("#submitButton").on('click', function (event) {
+    event.preventDefault();
+
+    // value from the input
+    var favList = $("#thingsList").val().trim();
+    // add to fav list
+    things.push(favList);
+
+    makingButton();
+
+  });
 
 
 
